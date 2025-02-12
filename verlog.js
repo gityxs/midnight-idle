750141473df8e9d1afc70116e1ecb4dbd69669fa
zhaolinxu <zhaolinxu@sina.cn>
2025-02-12 17:00:28 +0800

    0.3.8

---
+++ verlog.js
@@ -0,0 +1,98 @@
+var logs={
+
+	"Bug Report":{text:"Please report any bugs found <a href='https://www.reddit.com/r/incremental_games/comments/1ifp86x/midnight_idle_v03x_jezebel_chapter/' style='color:white;' target='_blank'>HERE</a><br>"},
+
+	"v0.3.8 - 11-Feb-2025":{text:""+
+	"- Fixed bug where after Legion defeat unable to end cycle.<br>"+
+	""},
+
+	"v0.3.7 - 10-Feb-2025":{text:""+
+	"- Repositioned footer icons to the top of the page, for accessibility.<br>"+
+	"- Added pause/resume button (can use keyboard spacebar as well).<br>"+
+	"- Allow keyboard button (C) to close popups, and (B) to begin journey.<br>"+
+	"- Allow keyboard access to (Q)-Blood, (W)-Sanity, (E)-SP Shop, (R)-DS Shop.<br>"+
+	"- Allow keyboard access to (K)-Combat Log, (L)-Story Log, (D)-Dev Log, (S)-Settings, (A)-About.<br>"+
+	"- Enabling Wallet Padlock now enters the Orc Shaman shop first (to get Black Market bonus) before leaving.<br>"+
+	"- Disabling Wallet Padlock now takes effect the next time your enter Orc Shaman shop, rather than next cycle.<br>"+
+	"- Fixed bug where Hourglass at 0 level (toggled from Enabled) doesn't set speed to 1x.<br>"+
+	"- Story log now include titles.<br>"+
+	"- Combat log now include defeats (not just victories), as well as HP, Blood, Sanity & Spirit stats.<br>"+
+	"- Orc Shaman shop is now accessible at the end of cycles, but you can still only spend DS at the shop.<br>"+
+	""},
+
+	"v0.3.6 - 8-Feb-2025":{text:""+
+	"- Balancing - reduced cost for Soul Hunter (from base 50 to base 30).<br>"+
+	"- Balancing - increase max level for Aspiration Band (from max 10 to max 20).<br>"+
+	"- Fixed bug where death by Blood and Sanity at same time resulted in infinite pop-ups.<br>"+
+	"- Fixed bug where volume control resets after new cycle.<br>"+
+	""},
+
+	"v0.3.5 - 7-Feb-2025":{text:""+
+	"- Added cloud save/load feature.<br>"+
+	"- Fixed bugged options when choosing to leave without absorbing advanced classes.<br>"+
+	"- Fixed flickering bug when fighting Snappy.<br>"+
+	""},
+
+	"v0.3.4 - 6-Feb-2025":{text:""+
+	"- Balancing - reduced cost for Oaken Shield, Titanium Spikes & Mind Shield.<br>"+
+	"- Added an X close button to top right of pop-ups.<br>"+
+	"- Remove exploration's sprint/map/navigate to be removed only after entering portal.<br>"+
+	"- Fixed log scrolling issue.<br>"+
+	"- Fixed disable story art to be remembered after restart.<br>"+
+	"- Skill tree indicates class active and class inherit.<br>"+
+	"- Fine-tuned Guerilla for Goblin Patrol, Goblin Lackey & Goblin King.<br>"+
+	""},
+
+	"v0.3.3 - 4-Feb-2025":{text:""+
+	"- Reduced story pop-ups when revisiting portal without Ancestral Dreams.<br>"+
+	"- Updated attack sequences/positioning.<br>"+
+	"- Allowed individual attacks to be toggled Auto or Manual at combat log screen.<br>"+
+	"- Hourglass can now be toggled after maxed (x1, x2, x3).<br>"+
+	"- Added Buying and Selling DS multiplier (x1, x10, x100).<br>"+
+	"- Fixed option to leave kobold without choosing Good or Evil.<br>"+
+	""},
+
+	"v0.3.2 - 3-Feb-2025":{text:""+
+	"- Fixed missing sanity reward for Bat Horror encounter.<br>"+
+	"- Fixed Ogre Smash encounter with both Skeletal Swordmaster & Grandmaster.<br>"+
+	"- Fixed disable story art option.<br>"+
+	"- Updated last form for SP shop discount when dying (in Killing Quarters), instead of just end of Legion cycle.<br>"+
+	"- Automates Consume for Clarity after Addiction upgrade.<br>"+
+	"- Added alternate option to leave Orc Shaman without Ancestral Dream activating.<br>"+
+	"- Tesla and Waze can now be toggled.<br>"+
+	"- Fix incorrect story text when accepting Vampire offer.<br>"+
+	""},
+
+	"v0.3.1 - 2-Feb-2025":{text:""+
+	"- Fixed various bugs related to transitioning saves from v2.<br>"+
+	"- Added option to import from text box (for mobile).<br>"+
+	"- Updated fix regarding the unlimited armored skeleton.<br>"+
+	"- Auto complete Freeflow & Clockwork if Bookmarked purchased.<br>"+
+	"- Fixed volume/mute reset when starting a new cycle.<br>"+
+	"- Updated Aspiration to allow DS bonus down to 2 decimal points.<br>"+
+	""},
+
+	"v0.3.0 - 2-Feb-2025":{text:""+
+	"- Added change log (finally).<br>"+
+	"- Added Jezebel chapter. Please assist to test and report bugs.<br>"+
+	"- Added option to turn off story art.<br>"+
+	"- Added hourglass, to increase master game speed (defeat blood gargoyle to get key).<br>"+
+	"- Paused auto proceed at second crossroad, if condition met to access new area.<br>"+
+	"- Added markers to indicate paths taken in last cycle.<br>"+
+	"- Added combat result and attacks page to view outside of combat mode.<br>"+
+	"- Base SP reward for cycles increased to 5 (from 1).<br>"+
+	"- Soul Sponge SP reward per level increased to 2 (from 1).<br>"+
+	"- Increased XP for all elite encounters in Legion chapter.<br>"+
+	"- Wolf natural response now auto all attacks, instead of the only 2 basic slash attacks (includes other new slashes).<br>"+
+	"- Slightly improved non-slash attacks (Acid Spit, Unholy Claw, Magic Dagger, Goblin Bomb).<br>"+
+	"- Almost all running actions and attacks can be paused by clicking on it.<br>"+
+	"- Turning off optional upgrades (e.g. Popup Blocker, Demoralize) and refreshing the page will keep the cost remain at zero.<br>"+
+	"- Option to fight rats/bats now only removed (if not already from bridge) after entering the final door, instead of upon reaching it.<br>"+
+	"- Animal planet now also auto fight the first optional rat & bat encounter when they first appear.<br>"+
+	"- Straight-forward now excludes fighting of Goblin Patrol, and uses Goblin Lie instead.<br>"+
+	"- Wolf Blood Banking reduced subsequent time requirement to 1s (from 5s).<br>"+
+	"- Fixed reward issue for Ooze Absorb Bat.<br>"+
+	"- Save data from 0.2.x are compatible for 0.3.x import/load.<br>"+
+	""},
+
+}
